98ad475330bd66cf1001153a64fea79482335a3a
pranta-barua007 <prantabarua247@gmail.com>
2023-05-09 19:27:19 +0600

    messenger clone using next 13

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    swcPlugins: [["next-superjson-plugin", {}]],
+  },
+  images: {
+    domains: [
+      "res.cloudinary.com",
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com",
+    ],
+  },
+  webpack: (config) => {
+    config.externals = [...config.externals, "bcrypt"];
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
